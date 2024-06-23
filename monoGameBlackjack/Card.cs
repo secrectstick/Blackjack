f74d270880993faaa754915f30a6a7461a91058f
secrectstick <2004ejp@gmail.com>
2024-06-23 15:41:15 -0400

    setup

---
+++ monoGameBlackjack/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace monoGameBlackjack
+{
+    internal struct Card
+    {
+        public int number;
+        public string suite;
+        public int position;
+
+        public Card(int position,string suite,int number)
+        {
+            this.number = number;
+            this.suite = suite;
+            this.position = position;
+        }
+    }
+}
---
+++ monoGameBlackjack/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monoGameBlackjack
+{
+    internal class Player
+    {
+
+
+        public Player() 
+        { 
+
+        
+        }
+
+
+        // update
+
+
+
+
+
+        //draw
+    }
+}
